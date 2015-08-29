6f0e44cdee056c6ed59bf0559730ceca6075e477
ddevogel <ddevogel@yahoo.com>
2015-08-29 01:38:32 +0000

    Public

--- src/app/components/city/city.jsx
+++ src/app/components/city/city.jsx
@@ -4,34 +4,35 @@ import CityStore from '../../stores/city.store'
 const ENTER_KEY_CODE = 13;
 
 class City extends Component {
+  
   constructor(props) {
     super(props);
     this.state = {name: props.name};
     this._save = this._save.bind(this);
+    this._onChange = this._onChange.bind(this);
+    this._onKeyDown = this._onKeyDown.bind(this);
   }
 
   render() {
     return <input 
             className="blip"
-            //id={this.props.id}
-            //value={this.props.name}
-            placeholder="City"
+            placeholder={this.props.placeholder}
+            onBlur={this._save}
             // onBlur={ () => { 
             //   console.log('here');
             //   this.props.onSave(this.state.value); 
             //   this.setState({name: ''});
             // }}
-            onBlur={this._save}
-            onChange={ (/*object*/ event) => {
-                  this.setState({name: event.target.value});
-            }}  
-            // onChange={this._onChange.bind(this)}
-            onKeyDown={ () => {
-              if (event.keyCode === ENTER_KEY_CODE) {
-                this._save();
-              }            
-            }}  
-            // onKeyDown={this._onKeyDown.bind(this)}
+            onChange={this._onChange}
+            // onChange={ (/*object*/ event) => {
+            //       this.setState({name: event.target.value});
+            // }}  
+            onKeyDown={this._onKeyDown}
+            // onKeyDown={ () => {
+            //   if (event.keyCode === ENTER_KEY_CODE) {
+            //     this._save();
+            //   }            
+            // }}  
             />
   }
   
@@ -40,24 +41,21 @@ class City extends Component {
     this.setState({name: ''});
   }
 
-  // _onChange(/*object*/ event) {
-  //   this.setState({
-  //     name: event.target.value
-  //   });
-  // }
+  _onChange(event) {
+    this.setState({ name: event.target.value });
+  }
 
-  // _onKeyDown(/*object*/ event) {
-  //   if (event.keyCode === ENTER_KEY_CODE) {
-  //     this._save();
-  //   }
-  // }
+  _onKeyDown(event) {
+    if (event.keyCode === ENTER_KEY_CODE) {
+      this._save();
+    }
+  }
 };
 
-
-  
 City.propTypes = {
   name:  PropTypes.string,
-  placeholder: PropTypes.string
+  placeholder: PropTypes.string,
+  onSave: PropTypes.func.isRequired,
 };
 
 City.defaultProps = {
